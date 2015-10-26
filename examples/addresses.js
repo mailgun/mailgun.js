b67f5f9b592aa06d21b0ae725f59b41598b308f4
Eddy Hernandez <eddy.hernandez@rackspace.com>
2015-10-25 20:34:02 -0700

    docs(addresses): update readme & add example

---
+++ examples/addresses.js
@@ -0,0 +1,20 @@
+// require('babel/register'); // uncomment this for node versions < 4
+
+var mailgun = require('../index');
+var mg = mailgun.client({
+  username: 'api',
+  key:  process.env.MAILGUN_API_KEY || '',
+  public_key: process.env.MAILGUN_PUBLIC_KEY || ''
+});
+
+mg.validate.get('Alice <alice@example.com>')
+  .then(data => console.log('validate: ', data))
+  .catch(err => console.log(err));
+
+mg.parse.get(['Alice <alice@example.com>', 'bob@example.com', 'example.com'])
+  .then(data => console.log('parse: without dns/esp checks', data))
+  .catch(err => console.log(err));
+
+mg.parse.get('Alice <alice@example.com>, bob@example.com, example.com', true)
+  .then(data => console.log('parse: with dns/esp checks', data))
+  .catch(err => console.log(err));
