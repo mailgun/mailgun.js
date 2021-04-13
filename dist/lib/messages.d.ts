481baa5eb572a179e7c43ee8ba5da9acd559464b
jatin-mlveda <jatin.parate@mlveda.com>
2021-04-13 18:22:54 +0530

    added types for messages.create function

--- dist/lib/messages.d.ts
+++ dist/lib/messages.d.ts
@@ -1,9 +1,33 @@
 import Request from "./request";
+declare type Attachment = {
+    data: ReadableStream;
+    filename: string;
+    contentType: string;
+    knownLength?: number;
+};
+declare type createData = {
+    to: string | string[];
+    from: string;
+    subject: string;
+    text: string;
+    attachment: (Attachment | Attachment[]);
+    'o:tag'?: string;
+    'o:campaign'?: any;
+    'o:deliverytime'?: string;
+    'o:dkim'?: 'yes' | 'no' | 'true' | 'false';
+    'o:testmode'?: 'yes';
+    'o:tracking'?: 'yes' | 'no' | 'true' | 'false';
+    'o:tracking-clicks'?: 'yes' | 'no' | 'true' | 'false';
+    'o:tracking-opens'?: 'yes' | 'no' | 'true' | 'false';
+    html?: string;
+    message?: string;
+} & Record<string, any>;
 export default class MessagesClient {
     request: Request;
     constructor(request: Request);
     _parseResponse(response: {
         body: any;
     }): any;
-    create(domain: string, data: any): Promise<any>;
+    create(domain: string, data: createData): Promise<any>;
 }
+export {};
--- lib/messages.ts
+++ lib/messages.ts
@@ -1,5 +1,30 @@
 import Request from "./request";
 
+type Attachment = {
+  data: ReadableStream,
+  filename: string,
+  contentType: string,
+  knownLength?: number,
+}
+
+type createData = {
+  to: string | string[],
+  from: string,
+  subject: string,
+  text: string,
+  attachment: (Attachment | Attachment[]),
+  'o:tag'?: string,
+  'o:campaign'?: any,
+  'o:deliverytime'?: string,
+  'o:dkim'?: 'yes' | 'no' | 'true' | 'false',
+  'o:testmode'?: 'yes',
+  'o:tracking'?: 'yes' | 'no' | 'true' | 'false',
+  'o:tracking-clicks'?: 'yes' | 'no' | 'true' | 'false',
+  'o:tracking-opens'?: 'yes' | 'no' | 'true' | 'false',
+  html?: string,
+  message?: string,
+} & Record<string, any>
+
 export default class MessagesClient {
   request: Request;
 
@@ -15,7 +40,7 @@ export default class MessagesClient {
     return response;
   }
 
-  create(domain: string, data: any) {
+  create(domain: string, data: createData) {
     if (data.message) {
       return this.request.postMulti(`/v3/${domain}/messages.mime`, data)
       .then(this._parseResponse);
