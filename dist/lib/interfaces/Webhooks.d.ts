ed1719da3225981c5fe56ae2d48fc4733eb2d7ae
Oleksandr Mazepa <zepa1993@gmail.com>
2021-08-04 13:12:49 +0300

    Fix: Error 400 on creating a webhook

---
+++ dist/lib/interfaces/Webhooks.d.ts
@@ -0,0 +1,21 @@
+export interface APIWebhook {
+    url?: string;
+    urls?: string[];
+}
+export interface WebhookResponseBody {
+    message: string;
+    webhook: APIWebhook;
+}
+export interface WebhookResponse {
+    status: string;
+    body: WebhookResponseBody;
+}
+export interface WebhookList {
+    [id: string]: {
+        urls: string[];
+    };
+}
+export interface WebhooksQuery {
+    limit?: number;
+    skip?: number;
+}
--- dist/lib/webhooks.d.ts
+++ dist/lib/webhooks.d.ts
@@ -1,24 +1,19 @@
+import { WebhookList, WebhookResponse, WebhooksQuery } from './interfaces/Webhooks';
 import Request from './request';
 declare class Webhook {
     id: string;
     url: string;
-    constructor(id: string, data: {
-        url: string;
-    });
+    constructor(id: string, url: string);
 }
 export default class WebhookClient {
     request: any;
     constructor(request: Request);
     _parseWebhookList(response: {
         body: {
-            webhooks: any;
+            webhooks: WebhookList;
         };
-    }): any;
-    _parseWebhookWithID(id: string): (response: {
-        body: {
-            webhook: any;
-        };
-    }) => Webhook;
+    }): WebhookList;
+    _parseWebhookWithID(id: string): (response: WebhookResponse) => Webhook;
     _parseWebhookTest(response: {
         body: {
             code: number;
@@ -28,10 +23,10 @@ export default class WebhookClient {
         code: number;
         message: string;
     };
-    list(domain: string, query: any): any;
-    get(domain: string, id: string): any;
-    create(domain: string, id: string, url: string, test: boolean): any;
-    update(domain: string, id: string, url: string): any;
-    destroy(domain: string, id: string): any;
+    list(domain: string, query: WebhooksQuery): Promise<WebhookList>;
+    get(domain: string, id: string): Promise<Webhook>;
+    create(domain: string, id: string, url: string, test?: boolean): Promise<Webhook>;
+    update(domain: string, id: string, url: string): Promise<Webhook>;
+    destroy(domain: string, id: string): Promise<Webhook>;
 }
 export {};
