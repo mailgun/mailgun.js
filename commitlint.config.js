262ce0ccd594bbcd97c9aa9ba5f11ad6ce6fc8c5
Oleksandr Mazepa <zepa1993@gmail.com>
2021-10-13 17:04:20 +0300

    feature: Update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['Breaking', 'Feature', 'Fix', 'Other']],
-    'type-case': [2, 'always', 'pascal-case'],
+    'type-enum': [2, 'always', ['breaking', 'feature', 'fix', 'other']],
     'subject-case': [2, 'always', 'sentence-case']
   },
 };
