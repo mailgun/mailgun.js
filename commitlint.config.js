8a09a735368bdd9df302ff791a752d948aa27054
Oleksandr Mazepa <zepa1993@gmail.com>
2021-10-13 16:59:33 +0300

    Feat: Update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['Breaking', 'Feature', 'Fix', 'Other']],
+    'type-enum': [2, 'always', ['Breaking', 'Feat', 'Fix', 'Other']],
     'type-case': [2, 'always', 'pascal-case'],
     'subject-case': [2, 'always', 'sentence-case']
   },
