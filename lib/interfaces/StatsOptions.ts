67e2f591bd5f7e435f2558a62b7e1525671d96a6
Oleksandr Mazepa <zepa1993@gmail.com>
2021-12-14 11:25:05 +0200

    fix: Update stats methods to allow an array of events

--- lib/interfaces/StatsOptions.ts
+++ lib/interfaces/StatsOptions.ts
@@ -1,15 +1,23 @@
- interface StatsOptions {
+export interface Stat {
+  time: string | Date,
+  delivered: {
+    smtp: number,
+    http: number,
+    total: number
+  }
+}
+
+export interface StatsOptions {
   start: string | Date;
   end: string | Date;
   resolution: string;
-  stats: {
-    time: string | Date,
-    delivered: {
-      smtp: number,
-      http: number,
-      total: number
-    }
-  }[];
+  stats: Stat[];
 }
 
-export default StatsOptions;
+export interface StatsQuery {
+  event: string | string[];
+  start: string | Date;
+  end: string | Date;
+  resolution: 'hour'| 'day' | 'month';
+  duration: string;
+}
--- lib/stats.ts
+++ lib/stats.ts
@@ -1,18 +1,18 @@
 import urljoin from 'url-join';
 import Request from './request';
-import StatsOptions from './interfaces/StatsOptions';
+import { StatsQuery, StatsOptions, Stat } from './interfaces/StatsOptions';
 
 class Stats {
   start: Date;
   end: Date;
   resolution: string;
-  stats: any;
+  stats: Stat[];
 
   constructor(data: StatsOptions) {
     this.start = new Date(data.start);
     this.end = new Date(data.end);
     this.resolution = data.resolution;
-    this.stats = data.stats.map(function (stat: { time: string | Date }) {
+    this.stats = data.stats.map(function (stat: Stat) {
       const res = { ...stat };
       res.time = new Date(stat.time);
       return res;
@@ -27,17 +27,36 @@ export default class StatsClient {
     this.request = request;
   }
 
-  _parseStats(response: { body: StatsOptions }) {
+  private prepareSearchParams(query: StatsQuery): Array<Array<string>> {
+    let searchParams = [];
+    if (typeof query === 'object' && Object.keys(query).length) {
+      searchParams = Object.entries(query).reduce((arrayWithPairs, currentPair) => {
+        const [key, value] = currentPair;
+        if (Array.isArray(value) && value.length) {
+          const repeatedProperty = value.map((item) => [key, item]);
+          return [...arrayWithPairs, ...repeatedProperty];
+        }
+        arrayWithPairs.push([key, value]);
+        return arrayWithPairs;
+      }, []);
+    }
+
+    return searchParams;
+  }
+
+  _parseStats(response: { body: StatsOptions }): Stats {
     return new Stats(response.body);
   }
 
-  getDomain(domain: string, query: any) {
-    return this.request.get(urljoin('/v3', domain, 'stats/total'), query)
+  getDomain(domain: string, query?: StatsQuery): Promise<Stats> {
+    const searchParams = this.prepareSearchParams(query);
+    return this.request.get(urljoin('/v3', domain, 'stats/total'), searchParams)
       .then(this._parseStats);
   }
 
-  getAccount(query: any) {
-    return this.request.get('/v3/stats/total', query)
+  getAccount(query?: StatsQuery): Promise<Stats> {
+    const searchParams = this.prepareSearchParams(query);
+    return this.request.get('/v3/stats/total', searchParams)
       .then(this._parseStats);
   }
 }
