224790c4c20466a2203856d703b51e7d4a03a195
Oleksandr Mazepa <omaze@softserveinc.com>
2022-04-11 14:19:46 +0300

    feature: Add validation methods to mailing list client

--- lib/lists.ts
+++ lib/lists.ts
@@ -3,7 +3,11 @@ import {
   ListsQuery,
   CreateUpdateList,
   DestroyedList,
-  MailingList
+  MailingList,
+  ValidationApiResponse,
+  StartValidationResult,
+  ValidationResult,
+  CancelValidationResult
 } from './interfaces/lists';
 import { IMailListsMembers } from './interfaces/mailListMembers';
 
@@ -16,6 +20,16 @@ export default class ListsClient {
     this.request = request;
     this.baseRoute = '/v3/lists';
     this.members = members;
+  }
+
+  private parseValidationResult(status: number, data: ValidationApiResponse): ValidationResult {
+    return {
+      status,
+      validationResult: {
+        ...data,
+        created_at: new Date(data.created_at * 1000) // add millisecond to Unix timestamp
+      }
+    } as ValidationResult;
   }
 
   list(query?: ListsQuery): Promise<MailingList[]> {
@@ -41,5 +55,31 @@ export default class ListsClient {
   destroy(mailListAddress: string): Promise<DestroyedList> {
     return this.request.delete(`${this.baseRoute}/${mailListAddress}`)
       .then((response) => response.body as DestroyedList);
+  }
+
+  validate(mailListAddress: string): Promise<StartValidationResult> {
+    return this.request.post(`${this.baseRoute}/${mailListAddress}/validate`, {})
+      .then((response) => ({
+        status: response.status,
+        ...response.body
+      }) as StartValidationResult);
+  }
+
+  validationResult(mailListAddress: string): Promise<ValidationResult> {
+    return this.request.get(`${this.baseRoute}/${mailListAddress}/validate`)
+      .then(
+        (response) => this.parseValidationResult(
+          response.status,
+           response.body as ValidationApiResponse
+        )
+      );
+  }
+
+  cancelValidation(mailListAddress: string): Promise<CancelValidationResult> {
+    return this.request.delete(`${this.baseRoute}/${mailListAddress}/validate`)
+      .then((response) => ({
+        status: response.status,
+        message: response.body.message
+      } as CancelValidationResult));
   }
 }
--- test/lists.test.ts
+++ test/lists.test.ts
@@ -5,7 +5,12 @@ import Request from '../lib/request';
 import ListsClient from '../lib/lists';
 import RequestOptions from '../lib/interfaces/RequestOptions';
 import MailListMembers from '../lib/mailListMembers';
-import { MailingList } from '../lib/interfaces/lists';
+import {
+  CancelValidationResult,
+  MailingList,
+  StartValidationResult,
+  ValidationResult
+} from '../lib/interfaces/lists';
 import { InputFormData } from '../lib/interfaces/IFormData';
 
 describe('ListsClient', function () {
@@ -94,6 +99,98 @@ describe('ListsClient', function () {
       });
       return client.update('test@example.com', { name: 'another name' }).then(function (data: any) {
         data.should.eql(defaultList);
+      });
+    });
+  });
+
+  describe('validate', function () {
+    it('start validation of the list', function () {
+      api.post('/v3/lists/test@example.com/validate').reply(200, {
+        id: 'testId',
+        message: 'test message',
+      });
+      return client.validate('test@example.com').then(function (data: StartValidationResult) {
+        data.should.eql({
+          status: 200,
+          id: 'testId',
+          message: 'test message',
+        });
+      });
+    });
+  });
+
+  describe('validationResult', function () {
+    it('returns correct result shape', function () {
+      api.get('/v3/lists/test@example.com/validate').reply(200, {
+        status: 'uploaded',
+        created_at: 1649665452,
+        download_url: {
+          csv: 'csvURL',
+          json: 'JSONURL'
+        },
+        id: 'testId',
+        quantity: 100,
+        records_processed: 100,
+        summary: {
+          result: {
+            catch_all: 0,
+            deliverable: 0,
+            do_not_send: 0,
+            undeliverable: 0,
+            unknown: 100
+          },
+          risk: {
+            high: 0,
+            low: 0,
+            medium: 0,
+            unknown: 100
+          }
+        }
+      });
+
+      return client.validationResult('test@example.com').then(function (data: ValidationResult) {
+        data.should.eql({
+          status: 200,
+          validationResult: {
+            status: 'uploaded',
+            created_at: new Date('2022-04-11T08:24:12.000Z'),
+            download_url: {
+              csv: 'csvURL',
+              json: 'JSONURL'
+            },
+            id: 'testId',
+            quantity: 100,
+            records_processed: 100,
+            summary: {
+              result: {
+                catch_all: 0,
+                deliverable: 0,
+                do_not_send: 0,
+                undeliverable: 0,
+                unknown: 100
+              },
+              risk: {
+                high: 0,
+                low: 0,
+                medium: 0,
+                unknown: 100
+              }
+            }
+          }
+        });
+      });
+    });
+  });
+  describe('cancelValidation', function () {
+    it('cancels validation process', function () {
+      api.delete('/v3/lists/test@example.com/validate').reply(200, {
+        message: 'test message'
+      });
+      return client.cancelValidation('test@example.com').then(function (data: CancelValidationResult) {
+        data.should.eql({
+          status: 200,
+          message: 'test message'
+        });
       });
     });
   });
