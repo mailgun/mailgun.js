a5a8897932cb2499f62aabaf31f6f5c8650236a5
Oleksandr Mazepa <omaze@softserveinc.com>
2022-07-26 15:51:34 +0300

    other: Update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['breaking', 'feature', 'fix', 'other']],
-    'subject-case': [2, 'always', 'sentence-case']
+    'subject-case': [2, 'always', 'sentence-case'],
+    'footer-max-line-length': [1, 'always', 200],
+    'body-max-line-length': [1, 'always', 1000]
   },
 };
