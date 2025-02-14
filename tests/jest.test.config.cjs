60aa11ba619d884cf416190f0c60c64328d26a26
Oleksandr Mazepa <zepa1993@gmail.com>
2025-02-14 12:43:43 +0200

    other: Setup coverage report

--- tests/jest.test.config.cjs
+++ tests/jest.test.config.cjs
@@ -2,12 +2,27 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 
 module.exports = {
+  rootDir: '../',
   testEnvironment: 'node',
   testMatch: ['**/module/*.test.ts'],
   transform: {
     '\\.[jt]sx?$': ['ts-jest', {
       useESM: true
     }],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/lib/Classes/**/*.*',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: '.coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    }
   },
   moduleNameMapper: {
     '^\\.(.+)\\.js': '.$1'
